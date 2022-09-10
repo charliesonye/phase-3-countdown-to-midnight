58c5d98cc13f9bc31084146e78bf49ffe72ba9de
Charles <c_onyemem@live.com>
2022-09-09 17:45:31 -0700

    Completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(int)
+   
+    x=0
+    while x  < int
+        puts "#{int - x} SECOND(S)!"
+        x += 1
+     def countdown_with_sleep(count_int)
+        sleep count_int
+     end
+        
+    end
+  "HAPPY NEW YEAR!"
 end
+
